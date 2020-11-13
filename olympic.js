3ffb3cf643eca8203f80333db05adea848191299
starbuckshiker <u1304954@umail.utah.edu>
2020-11-12 20:26:18 -0700

    they are equal!

---
+++ olympic.js
@@ -0,0 +1,30 @@
+
+if(5 > 3) {
+    console.log(" 5 is greater than 3")
+}
+
+}
+if("cat" == 3) {
+    console.log(" 3 is the length of a cat")
+}    else if (cat === cat){
+    console.log("It's a Cat")
+}    else (cat === dog){
+    console.log("not the same")
+}
+
+
+
+
+var people = {
+      name: "Boby",
+      age: 12,
+      gender: "male"
+    }
+    for(let x = 0; x < people.length; x++) {
+        if(people[x].age >= 18){
+           console.log(people[x].name + " is allowed to go to the movie");
+        } else if ()
+        else {
+           console.log(people[x].name + " is not allowed to go to the movie");
+        } 
+   }
