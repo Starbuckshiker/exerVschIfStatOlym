d3315398d5e9a197aaccbcd18dd5760c0db6607e
starbuckshiker <u1304954@umail.utah.edu>
2020-11-12 22:49:29 -0700

    silver

--- olympic.js
+++ olympic.js
@@ -3,18 +3,18 @@ if(5 > 3) {
     console.log(" 5 is greater than 3")
 }
 
-}
 if("cat" == 3) {
     console.log(" 3 is the length of a cat")
-}    else if (cat === cat){
+} else if (cat === cat){
     console.log("It's a Cat")
-}    else (cat === dog){
+} else if (cat === dog){
     console.log("not the same")
-}
+    } else {
+        console.log("pick a pet, would you!")}
 
 
 
-
+ // Bronze
 var people = {
       name: "Boby",
       age: 12,
@@ -23,8 +23,39 @@ var people = {
     for(let x = 0; x < people.length; x++) {
         if(people[x].age >= 18){
            console.log(people[x].name + " is allowed to go to the movie");
-        } else if ()
+        } else if (people[x].age >= 18 && people.name == B + "is allowed to go to the movie") {
+            console.log(people[x].name + " is allowed to go to the movie");
+        }
         else {
-           console.log(people[x].name + " is not allowed to go to the movie");
+           console.log(people[x].name + " is not allowed to go to the movie") 
         } 
    }
+
+ // Silver
+if (1 === 1){
+    console.log("strict")
+} else if(1 == 1) {
+    console.log("loose" || "abstract")
+} else { 
+    console.log(" not equal at all" )}
+if (1 <= 2 && 2 === 4) {
+    console.log("yes")
+} else {" your math don't add up"
+}
+ // Gold
+ var colors = ["dog", "cat", "tiger", "wolf"]
+ if ("blue") {
+     console.log("Its a dog")
+     }
+ if (True){
+     console.log("It's true")
+ }       
+
+if (typeof variable !== "undefined") {
+    console.log("undefined")
+}
+
+ if ("s" >= 12){
+     console.log("Wow that's not right")
+     } else {"that was a dumb question"
+     }
