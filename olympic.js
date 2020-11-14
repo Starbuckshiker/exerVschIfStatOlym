cf8e0d6195e03e7dc792d2997ef7546524285633
starbuckshiker <u1304954@umail.utah.edu>
2020-11-14 14:43:39 -0700

    it true!

--- olympic.js
+++ olympic.js
@@ -1,16 +1,15 @@
 
 if(5 > 3) {
-    console.log(" 5 is greater than 3")
+    console.log(" is greater than ")
 }
-
-if("cat" == 3) {
-    console.log(" 3 is the length of a cat")
-} else if (cat === cat){
-    console.log("It's a Cat")
-} else if (cat === dog){
+var cat = 3
+if(cat === 3) {
+    console.log(" is the length ")
+}
+if (cat !== "dog"){
     console.log("not the same")
     } else {
-        console.log("pick a pet, would you!")}
+        console.log("not the same")}
 
 
 
@@ -18,18 +17,23 @@ if("cat" == 3) {
 var people = {
       name: "Boby",
       age: 12,
-      gender: "male"
-    }
-    for(let x = 0; x < people.length; x++) {
-        if(people[x].age >= 18){
-           console.log(people[x].name + " is allowed to go to the movie");
-        } else if (people[x].age >= 18 && people.name == B + "is allowed to go to the movie") {
-            console.log(people[x].name + " is allowed to go to the movie");
-        }
-        else {
-           console.log(people[x].name + " is not allowed to go to the movie") 
+          }
+if (people.age >= 18){
+    console.log(people.name + " is allwed to go the movie")
+} else {
+    console.log(people.name + " is not allowed to go the movie")
+}
+if (people.name[0] === "B"){
+    console.log(people.name + " is allowed to go to the movie");
+ } else {
+    console.log(people.name + " is not allowed to go to the movie"); 
+ } 
+if (people.age >= 18 && people.name === "B"){
+           console.log(people.name + " is allowed to go to the movie");
+        } else {
+           console.log(people.name + " is not allowed to go to the movie"); 
         } 
-   }
+   
 
  // Silver
 if (1 === 1){
@@ -40,14 +44,15 @@ if (1 === 1){
     console.log(" not equal at all" )}
 if (1 <= 2 && 2 === 4) {
     console.log("yes")
-} else {" your math don't add up"
+} else {
+    console.log("no")
 }
  // Gold
- var colors = ["dog", "cat", "tiger", "wolf"]
- if ("blue") {
+ var animals = ["dog", "cat", "tiger", "wolf"]
+ if ("dog") {
      console.log("Its a dog")
      }
- if (True){
+ if (true){
      console.log("It's true")
  }       
 
@@ -57,5 +62,6 @@ if (typeof variable !== "undefined") {
 
  if ("s" >= 12){
      console.log("Wow that's not right")
-     } else {"that was a dumb question"
+     } else {
+         console.log( "that was a dumb question")
      }
